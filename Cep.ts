c4254955f11ceb002a78cdae852c067969a8485a
andremc331 <151594451+andremc331@users.noreply.github.com>
2024-08-27 21:48:18 -0300

    Update Cep.ts

--- Cep.ts
+++ Cep.ts
@@ -6,3 +6,17 @@ export default async function getCep(nro:string){
 }
 
 // https://viacep.com.br/ws/12328070/json
+
+//////////////////////////////////////////////
+
+import { api } from "./api";
+
+class Cep {
+    async get(nro:string){
+    const {data} = await api.get(`/${nro}/json`);
+    return data;
+    }
+}
+
+const objeto = new Cep();
+export default objeto;
