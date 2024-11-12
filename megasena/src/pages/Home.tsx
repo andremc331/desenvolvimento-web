421bb68458346eb490b05adff6744488fd3db90c
andremc331 <andremc331@gmail.com>
2024-11-12 19:31:13 -0300

    finish

--- megasena/src/pages/Home.tsx
+++ megasena/src/pages/Home.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import useLottery from '../hooks/useLottery';
-import LotteryResult from '../components/LotteryResultProps';
-
+import React from "react";
+import useLottery from "../hooks/useLottery";
+import LotteryResult from "../components/LotteryResultProps";
+import styled from "styled-components";
 
 const Home: React.FC = () => {
   const { megasena, loading, error } = useLottery();
 
   // Exibe mensagem de carregamento enquanto os dados estão sendo carregados
   if (loading) {
-    return <div>Carregando...</div>;
+    return <Loading>Carregando...</Loading>;
   }
 
   // Exibe mensagem de erro caso a requisição falhe
@@ -18,10 +18,26 @@ const Home: React.FC = () => {
 
   // Exibe os resultados quando os dados são carregados
   return (
-    <div>
+    <Resultado>
       {megasena && <LotteryResult megasena={megasena.megasena} />}
-    </div>
+    </Resultado>
   );
 };
 
 export default Home;
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.5rem;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 1.5rem;
+`;
